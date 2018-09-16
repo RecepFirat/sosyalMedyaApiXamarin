c19fd15ac75f0c5753b30eabb62a0766cccb8d1b
recep tayyip fırat <rtf3444@gmail.com>
2018-09-17 01:50:21 +0300

    ilerledin badrequest hatasında tıkandın videoda 31.14 dakıkadasın

---
+++ XamarinApi/XamarinApi/XamarinApi/Provider/ServiceManager.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinApi.Provider
+{
+    public class ServiceManager<T> where T : class //generic bi yapı oldu cunku hep twitterda hem facede hemde instada kullanabıleyım
+    {
+        private async Task<HttpClient> GetClient()
+        {
+            HttpClient client = new HttpClient();
+            return client;
+        }
+
+        //buradada  isterler gerceklestirip ona gore islem yapıcam
+
+        public async Task<T> GetFacebookProfile(string accessToken)
+        {
+
+            var httpclient = await GetClient();
+
+            var requestURL = "https://graph.facebook.com/v2.7/me?fields=name,picture,work,website,religion,location,locale,link,cover,age_range,birthday,devices,email,first_name,last_name,gender,hometown,is_verified,languages&access_token="
+                    + accessToken;
+            var userJson = await httpclient.GetStringAsync(requestURL);
+            var profile = JsonConvert.DeserializeObject<T>(userJson);//hangi claassta yaratıldıysa kendını ona cast edicek
+            return profile;
+        }
+    }
+}
--- XamarinApi/XamarinApi/XamarinApi/Views/FacebookPage.cs
+++ XamarinApi/XamarinApi/XamarinApi/Views/FacebookPage.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
+using XamarinApi.Models;
+using XamarinApi.Provider;
 
 namespace XamarinApi.Views
 {
@@ -20,7 +22,42 @@ namespace XamarinApi.Views
                 Source = apiRequest,
                 HeightRequest = 1
             };
+            webview.Navigated += Webview_Navigated;
             Content = webview;
+
+        }
+    //    EAAHFK5ZB4ilgBALVAXJZCcVg1i6HxLB4u10bjoZCZCA6yiw5XLiuFPKr5vg41QHTCwa4GPy1MWturubmNHTbU0ZBxIe6mRhBhIS59ff9yPrYgTkndYRA1RA6X7G55U6135H5k7Mgw0ciIXnpZBZCzqUIXiqWENJ4CgH5lrShGslTRdZBSJMNvBuGJEWj1liIoxsZD
+        private async void Webview_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            var accesstoken = ExtractAccestokenFromUri(e.Url);
+
+            if (!string.IsNullOrEmpty(accesstoken))
+            {
+                //artık get istegi bu tipte bana cevap vericek
+                ServiceManager<FacebookProfile> manager = new ServiceManager<FacebookProfile>();
+                var profile =  await manager.GetFacebookProfile(accesstoken);
+
+                var name = profile.Name;
+                //burada sımdı bı post ıslemı gerceklestırıp bunun uzerınde ıslem yapıcaz
+            }
+        }
+        private string ExtractAccestokenFromUri(string uri)
+        {
+
+            if (uri.Contains("access_token")&&uri.Contains("&expires_in="))
+            {
+                var https = "https";
+                if (Xamarin.Forms.Device.OS==TargetPlatform.Windows|| Xamarin.Forms.Device.OS==TargetPlatform.WinPhone)
+                {
+                    https = "https";
+                }
+                uri = uri.Replace(https + "://www.facebook.com/connect/login.success.html#access_token=", "");
+                var accesstoken = uri.Remove(uri.IndexOf("&expires_in"));
+                return accesstoken;
+
+            }
+            return "";
         }
     }
+    
 }
